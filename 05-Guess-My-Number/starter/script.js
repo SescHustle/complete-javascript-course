54fc264aa34cd044c2cf281bdf1f7c83a9809113
Pavel Lovkii <plovkiy@yandex.ru>
2022-03-08 16:56:12 +0700

    feat: 05 - CC1 - add game reset and highscore

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,9 +1,10 @@
 'use strict';
-const secretNumber = Number(Math.trunc(Math.random() * 20)) + 1;
+let secretNumber = Number(Math.trunc(Math.random() * 20)) + 1;
 let score = 20;
+let highscore = 0;
 
 
-document.querySelector('.check').addEventListener('click', function() {
+document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
 
     // No input.
@@ -34,9 +35,21 @@ document.querySelector('.check').addEventListener('click', function() {
 
     // Player wins.
     if (guess === secretNumber) {
+        highscore = (score > highscore) ? score : highscore;
+        document.querySelector('.highscore').textContent = highscore;
         document.querySelector('.message').textContent = 'Correct Number!';
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
         document.querySelector('.number').textContent = secretNumber;
     }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.number').textContent = '?';
+    score = 20;
+    document.querySelector('.guess').value = null;
+    document.querySelector('.score').textContent = score;
+    secretNumber = Number(Math.trunc(Math.random() * 20)) + 1;
 });
