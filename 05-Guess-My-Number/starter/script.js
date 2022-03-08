074d858604369b3e473bd69a7f29cbc29c8ca744
Pavel Lovkii <plovkiy@yandex.ru>
2022-03-08 18:19:38 +0700

    chore: 05 - refactoring

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -3,53 +3,75 @@ let secretNumber = Number(Math.trunc(Math.random() * 20)) + 1;
 let score = 20;
 let highscore = 0;
 
+const messageUpdate = function (message) {
+    document.querySelector('.message').textContent = message;
+}
+const scoreUpdate = function (score) {
+    document.querySelector('.score').textContent = score;
+}
+const highscoreUpdate = function (highscore) {
+    document.querySelector('.highscore').textContent = highscore;
+}
+const getGuess = function () {
+    return Number(document.querySelector('.guess').value);
+}
+
+const playAgain = function () {
+    stylesUpdate(false);
+    secretNumber = resetValues();
+}
+
+const stylesUpdate = function (win = true) {
+    let backgroundColor = '#60b347';
+    let size = '30rem';
+    let textContent = secretNumber;
+    if (!win) {
+        backgroundColor = '#222';
+        size = '15rem';
+        textContent = '?';
+    }
+    document.querySelector('body').style.backgroundColor = backgroundColor;
+    document.querySelector('.number').style.width = size;
+    document.querySelector('.number').textContent = textContent;
+}
+
+const resetValues = function () {
+    // Reset values.
+    score = 20;
+    document.querySelector('.guess').value = null;
+    document.querySelector('.score').textContent = score;
+
+    // Return new secret number.
+    return Number(Math.trunc(Math.random() * 20)) + 1;
+}
+
+document.querySelector('.again').addEventListener('click', playAgain);
 
 document.querySelector('.check').addEventListener('click', function () {
-    const guess = Number(document.querySelector('.guess').value);
-
+    const guess = getGuess();
+    
     // No input.
     if (!guess) {
-        document.querySelector('.message').textContent = 'No number provided!';
+        messageUpdate('No number provided!');
         return;
     }
 
     // Comparison
-    if (guess > secretNumber) {
+    if (guess !== secretNumber) {
         if (--score <= 0) {
-            document.querySelector('.message').textContent = 'You lost!';
-            document.querySelector('.score').textContent = 0;
+            messageUpdate('You lost!');
+            scoreUpdate(0);
             return;
         }
-        document.querySelector('.message').textContent = 'Too high!';
-        document.querySelector('.score').textContent = score;
-    }
-    if (guess < secretNumber) {
-        if (--score <= 0) {
-            document.querySelector('.message').textContent = 'You lost!';
-            document.querySelector('.score').textContent = 0;
-            return;
-        }
-        document.querySelector('.message').textContent = 'Too low!';
-        document.querySelector('.score').textContent = score;
+        let message = guess > secretNumber ? 'Too high!' : 'Too low!';
+        messageUpdate(message);
+        scoreUpdate(score);
+        return;
     }
 
     // Player wins.
-    if (guess === secretNumber) {
-        highscore = (score > highscore) ? score : highscore;
-        document.querySelector('.highscore').textContent = highscore;
-        document.querySelector('.message').textContent = 'Correct Number!';
-        document.querySelector('body').style.backgroundColor = '#60b347';
-        document.querySelector('.number').style.width = '30rem';
-        document.querySelector('.number').textContent = secretNumber;
-    }
-});
-
-document.querySelector('.again').addEventListener('click', function () {
-    document.querySelector('body').style.backgroundColor = '#222';
-    document.querySelector('.number').style.width = '15rem';
-    document.querySelector('.number').textContent = '?';
-    score = 20;
-    document.querySelector('.guess').value = null;
-    document.querySelector('.score').textContent = score;
-    secretNumber = Number(Math.trunc(Math.random() * 20)) + 1;
+    highscore = (score > highscore) ? score : highscore;
+    highscoreUpdate(highscore);
+    messageUpdate('Correct Number!');
+    stylesUpdate(true);
 });
