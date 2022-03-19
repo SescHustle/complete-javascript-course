bfda82662ee2889cbf0bf7f60ab21edc58134ccb
Pavel Lovkii <plovkiy@yandex.ru>
2022-03-19 18:36:23 +0700

    feat: 11 - Coding Challenge 1

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,23 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+// Coding Challenge #1
+const checkDogs = function(dogs1, dogs2) {
+  // 1.
+  dogs1.splice(0, 1);
+  dogs1.splice(-2);
+  // 2.
+  const allDogs = dogs1.concat(dogs2);
+  // 3.
+  allDogs.forEach((dogAge, i) => {
+    dogAge >= 3 ?
+    console.log(`Dog number ${i + 1} is an adult, and is ${dogAge} years old`) :
+    console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+  });
+}
+// 4.
+console.log('---- TEST DATA 1 ----');
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+console.log('---- TEST DATA 2 ----');
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
