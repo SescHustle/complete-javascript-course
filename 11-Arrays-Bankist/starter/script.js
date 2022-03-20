bbef724220614a558069c547c44cff8170438c92
Pavel Lovkii <plovkiy@yandex.ru>
2022-03-20 20:31:12 +0700

    feat: 11 - Coding Challenge 4

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -38,6 +38,52 @@ calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 console.log('---- TEST DATA 2 ----');
 calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 
+// Coding Challenge #4
+console.log('---- CODING CHALLENGE #4 ----');
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] }
+];
+// 1.
+dogs.forEach(dog => dog.recFood =  Math.trunc(dog.weight ** 0.75 * 28));
+console.log(dogs);
+
+// 2.
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+console.log(
+  `Sarah's dog is eating too ${
+    dogSarah.curFood > dogSarah.recFood ? 'much' : 'little'
+  } `
+);
+
+// 3.
+const ownersEatTooMuch = dogs.filter(dog => dog.curFood >= dog.recFood).flatMap(dog => dog.owners);
+const ownersEatTooLittle = dogs
+  .filter(dog => dog.curFood < dog.recFood)
+  .flatMap(dog => dog.owners);
+console.log(ownersEatTooLittle);
+console.log(ownersEatTooMuch);
+
+// 4.
+console.log(`${ownersEatTooLittle.join(' and ')} dogs are eat too little!`);
+console.log(`${ownersEatTooMuch.join(' and ')} dogs are eat too much!`);
+
+// 5.
+console.log(dogs.some(dog => dog.curFood === dog.recFood));
+
+// 6.
+const checkEatingOkay = dog => dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1;
+console.log(dogs.some(checkEatingOkay));
+
+// 7.
+const dogsEatingOkay = dogs.filter(checkEatingOkay);
+console.log(dogsEatingOkay);
+
+// 8.
+const dogsSorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
+console.log(dogsSorted);
 /////////////////////////////////////////////////
 // BANKIST APP
 
