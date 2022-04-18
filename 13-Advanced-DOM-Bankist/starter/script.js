5f4cba5d70a5bd9e5d62c8dc75ee8cc0a911a547
Pavel Lovkii <plovkiy@yandex.ru>
2022-04-18 22:15:22 +0700

    feat: 13 - Bankist Web - implement slider

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -143,3 +143,69 @@ const imgObserver = new IntersectionObserver(loadImg,{
 });
 
 imgTargets.forEach(img => imgObserver.observe(img));
+
+// Slider
+const initSlider = function () {
+  const slides = document.querySelectorAll('.slide');
+  const btnLeft = document.querySelector('.slider__btn--left');
+  const btnRight = document.querySelector('.slider__btn--right');
+  const dotContainer = document.querySelector('.dots');
+  
+  let currentSlide = 0;
+  const maxSlide = slides.length;
+  
+  const goToSlide = function (slide) {
+    slides.forEach((s, i) => {
+      s.style.transform = `translateX(${100 * (i - slide)}%)`;
+    });
+  }
+
+  const createDots = function () {
+    slides.forEach((_, i) => {
+      dotContainer.insertAdjacentHTML(
+        'beforeend',
+        `<button class="dots__dot" data-slide="${i}"></>`)
+    });
+  }
+
+  const activateDot = function(slide) {
+    document.querySelectorAll('.dots__dot').forEach(dot => {
+      dot.classList.remove('dots__dot--active');
+    });
+    document.querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add('dots__dot--active');
+  }
+  
+  const prevSlide = function() {
+    if (currentSlide === 0) {
+      currentSlide = maxSlide - 1;
+    } else {
+      currentSlide--;
+    }
+    goToSlide(currentSlide);
+    activateDot(currentSlide);
+  }
+  
+  const nextSlide = function() {
+    if (currentSlide === maxSlide - 1) {
+      currentSlide = 0;
+    } else {
+      currentSlide++;
+    }
+    goToSlide(currentSlide);
+    activateDot(currentSlide);
+  }
+  
+  goToSlide(currentSlide);
+  createDots();
+  activateDot(currentSlide);
+  
+  btnRight.addEventListener('click', nextSlide);
+  btnLeft.addEventListener('click', prevSlide);
+  document.addEventListener('keydown', e => {
+    e.key === 'ArrowRight' && nextSlide();
+    e.key === 'ArrowLeft' && prevSlide();
+  });
+
+}
+initSlider();
