e003b5f97e7b848466891b64640e6e393081eba8
Pavel Lovkii <plovkiy@yandex.ru>
2022-05-28 16:44:27 +0700

    feat: 16 - Async - coding challenges 2 and 3

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -27,26 +27,127 @@ console.log('------ CODING CHALLENGE #1 ------');
 const whereAmI = function (lat, lng) {
   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
     .then(response => {
-        if (!response.ok) throw Error(`Can not get location by coordinates. Error: ${response.status}`);
-        return response.json()
+      if (!response.ok)
+        throw Error(
+          `Can not get location by coordinates. Error: ${response.status}`
+        );
+      return response.json();
     })
     .then(data => {
       console.log(`You are in ${data.city}, ${data.country}.`);
       return fetch(`https://restcountries.com/v2/name/${data.country}`);
     })
     .then(response => {
-        if (!response.ok) throw Error(`Unable to fetch country. Error: ${response.status}`);
-        return response.json()
+      if (!response.ok)
+        throw Error(`Unable to fetch country. Error: ${response.status}`);
+      return response.json();
     })
     .then(country => renderCountry(country[0]))
     .catch(err => console.log(err.message));
-}
-navigator.geolocation
-  .getCurrentPosition(
-    position => whereAmI(
-      position.coords.latitude,
-      position.coords.longitude
-    ));
+};
+navigator.geolocation.getCurrentPosition(position =>
+  whereAmI(position.coords.latitude, position.coords.longitude)
+);
 // whereAmI(52.508, 13.381);
 // whereAmI(19.037, 72.873);
 // whereAmI(-33.933, 18.474);
+
+// Coding challenge #2
+console.log('------ CODING CHALLENGE #2 ------');
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const imgContainer = document.querySelector('.images');
+
+const createImage = function (path) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = path;
+
+    img.addEventListener('load', function () {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', function () {
+      reject(new Error('Image not found'));
+    });
+  });
+};
+
+// let currentImg;
+// createImage('img/img-1.jpg')
+//   .then(img => {
+//     currentImg = img;
+//     console.log('Image 1 loaded');
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//     return createImage('img/img-2.jpg');
+//   })
+//   .then(img => {
+//     currentImg = img;
+//     console.log('Image 2 loaded');
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//   })
+//   .catch(err => {
+//     console.error(err);
+//   });
+
+// Coding challenge #3
+console.log('------ CODING CHALLENGE #3 ------');
+
+/* 
+PART 1
+Write an async function 'loadNPause' that recreates Coding Challenge #2, this time using async/await (only the part where the promise is consumed). Compare the two versions, think about the big differences, and see which one you like more.
+Don't forget to test the error handler, and to set the network speed to 'Fast 3G' in the dev tools Network tab.
+
+PART 2
+1. Create an async function 'loadAll' that receives an array of image paths 'imgArr';
+2. Use .map to loop over the array, to load all the images with the 'createImage' function (call the resulting array 'imgs')
+3. Check out the 'imgs' array in the console! Is it like you expected?
+4. Use a promise combinator function to actually get the images from the array 😉
+5. Add the 'paralell' class to all the images (it has some CSS styles).
+
+TEST DATA: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']. To test, turn off the 'loadNPause' function.
+
+GOOD LUCK 😀
+*/
+
+const loadNPause = async function () {
+  try {
+    let img = await createImage('img/img-1.jpg');
+    console.log('Image 1 loaded');
+    await wait(2);
+    img.style.display = 'none';
+
+    img = await createImage('img/img-2.jpg');
+    console.log('Image 1 loaded');
+    await wait(2);
+    img.style.display = 'none';
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+//loadNPause();
+
+const loadAll = async function (imgArr) {
+  try {
+    const images = imgArr.map(async img => await createImage(img));
+    const imgsEl = await Promise.all(images);
+    console.log(imgsEl);
+    imgsEl.forEach(img => img.classList.add('parallel'));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
